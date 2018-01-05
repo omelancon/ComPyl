be88bc1d17fd7be434473f7c066259b4ca35b150
omelancon <ol.melancon@gmail.com>
2018-01-05 18:59:40 -0500

    bump minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='compyl',
       packages=find_packages(exclude=("tests",)),
-      version='0.1.0',
+      version='0.1.1',
       description='Python lexing-parsing tool',
       author='Olivier Melancon',
       author_email='ol.melancon@gmail.com',
