d079a5bbba1dea1b7c54d1d6697e6038e829f72b
omelancon <ol.melancon@gmail.com>
2018-01-05 18:41:35 -0500

    fix setup packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='compyl',
-      packages=['compyl'],
+      packages=find_packages(exclude=("tests",)),
       version='0.1.0',
       description='Python lexing-parsing tool',
       author='Olivier Melancon',
