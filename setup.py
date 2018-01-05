8740ffa85ffed4a2ed11075a73e9198cef152750
omelancon <ol.melancon@gmail.com>
2018-01-05 17:34:18 -0500

    setup files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='ComPyl',
+      version='0.1.0',
+      description='Python lexing-parsing tool',
+      author='Olivier Melancon',
+      author_email='ol.melancon@gmail.com',
+      url='https://github.com/omelancon/ComPyl',
+      download_url='https://github.com/omelancon/ComPyl/archive/v0.1.0.tar.gz',
+      keywords='lexer lexing parser parsing compiler',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Software Development :: Compilers',
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+      ]
+      )
