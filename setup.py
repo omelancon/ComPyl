6b7224a58da680bb9b31ca33d2fa6fc9d008e54d
omelancon <ol.melancon@gmail.com>
2018-01-05 17:56:36 -0500

    add pachages to setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 
-setup(name='ComPyl',
+setup(name='compyl',
+      packages=['compyl'],
       version='0.1.0',
       description='Python lexing-parsing tool',
       author='Olivier Melancon',
