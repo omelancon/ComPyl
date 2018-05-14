c31228aae11adc13fdc766294d6740627a99658f
omelancon <ol.melancon@gmail.com>
2018-05-14 16:03:18 -0400

    convert test, fix minor bug where terminal tokens could not be given as list

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,8 +2,8 @@ import unittest
 
 import copy
 from compyl.__parser.finite_automaton import Token
-from compyl.parser import Parser, ParserError, ParserBuildError, ParserSyntaxError, GrammarError
-
+from compyl.parser import ParserError, ParserBuildError, ParserSyntaxError, GrammarError
+from compyl.parser import Parser as P
 
 # =========================================================
 # Helper tokens and functions
@@ -67,16 +67,15 @@ class ParserTestBasic(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
 
-        rules = {
-            'start': [
-                ('TOKEN_A TOKEN_B end?', Start),
-                ('TOKEN_A TOKEN_C end?', Start),
-            ],
-            'end': [('TOKEN_C TOKEN_D', End)]
-        }
+        class Parser(P):
+            terminal = 'start'
 
-        cls.parser = Parser(rules=rules, terminal='start')
-        cls.parser.build()
+            start =\
+                ('TOKEN_A TOKEN_B end?', Start),\
+                ('TOKEN_A TOKEN_C end?', Start)
+            end = ('TOKEN_C TOKEN_D', End)
+
+        cls.parser = Parser()
 
         cls.parser_copy = copy.deepcopy(cls.parser)
 
@@ -147,28 +146,24 @@ class ParserTestBasic(unittest.TestCase):
 class ParserTestAdvanced(unittest.TestCase):
 
     def test_advanced_grammar(self):
-        rules = {
-            'A': [
-                ('B A', get_string_of_instructions_reducer('A1')),
-                ('C A', get_string_of_instructions_reducer('A2')),
-                ('e', get_string_of_instructions_reducer('A3'))
-            ],
-            'B': [
-                ('a b c', get_string_of_instructions_reducer('B1')),
-                ('b c? a', get_string_of_instructions_reducer('B2'))
-            ],
-            'C': [
-                ('x Y B', get_string_of_instructions_reducer('C1')),
-                ('x y d', get_string_of_instructions_reducer('C2'))
-            ],
-            'Y': [
-                ('', get_string_of_instructions_reducer('Y1')),
-                ('y', get_string_of_instructions_reducer('Y2')),
-            ],
-        }
+        class Parser(P):
+            terminal = 'A'
 
-        parser = Parser(rules=rules, terminal='A')
-        parser.build()
+            A =\
+                ('B A', get_string_of_instructions_reducer('A1')),\
+                ('C A', get_string_of_instructions_reducer('A2')),\
+                ('e', get_string_of_instructions_reducer('A3'))
+            B =\
+                ('a b c', get_string_of_instructions_reducer('B1')),\
+                ('b c? a', get_string_of_instructions_reducer('B2'))
+            C =\
+                ('x Y B', get_string_of_instructions_reducer('C1')),\
+                ('x y d', get_string_of_instructions_reducer('C2'))
+            Y =\
+                ('', get_string_of_instructions_reducer('Y1')),\
+                ('y', get_string_of_instructions_reducer('Y2'))
+
+        parser = Parser()
 
         tokens = generate_token_stream(
             ('x', 'x'),
@@ -198,18 +193,17 @@ class ParserTestAdvanced(unittest.TestCase):
         reduce C1, END1, B1 when lookahead it e.
         """
 
-        rules = {
-            'A': [('B e', get_string_of_instructions_reducer('A1'))],
-            'B': [('C END', get_string_of_instructions_reducer('B1'))],
-            'C': [('c', get_string_of_instructions_reducer('C1'))],
-            'END': [
-                ('', get_string_of_instructions_reducer('END1')),
-                ('end', get_string_of_instructions_reducer('END2'))
-            ]
-        }
+        class Parser(P):
+            terminal = 'A'
 
-        parser = Parser(rules=rules, terminal='A')
-        parser.build()
+            A = 'B e', get_string_of_instructions_reducer('A1')
+            B = 'C END', get_string_of_instructions_reducer('B1')
+            C = 'c', get_string_of_instructions_reducer('C1')
+            END =\
+                ('', get_string_of_instructions_reducer('END1')),\
+                ('end', get_string_of_instructions_reducer('END2'))
+
+        parser = Parser()
 
         tokens = generate_token_stream(('c', 'c'), ('e', 'e'))
 
@@ -223,15 +217,15 @@ class ParserTestAdvanced(unittest.TestCase):
 
     def test_sequence_of_empty_rules(self):
 
-        rules = {
-            'start': [('A B C', get_string_of_instructions_reducer('start1'))],
-            'A': [('', get_string_of_instructions_reducer('A1'))],
-            'B': [('', get_string_of_instructions_reducer('B1'))],
-            'C': [('c?', get_string_of_instructions_reducer('C1'))],
-        }
+        class Parser(P):
+            terminal = 'start'
 
-        parser = Parser(rules=rules, terminal='start')
-        parser.build()
+            start = 'A B C', get_string_of_instructions_reducer('start1')
+            A = '', get_string_of_instructions_reducer('A1')
+            B = '', get_string_of_instructions_reducer('B1')
+            C = 'c?', get_string_of_instructions_reducer('C1')
+
+        parser = Parser()
 
         res = parser.end()
         expected = 'A1 B1 None C1 start1'
@@ -249,13 +243,13 @@ class ParserTestAdvanced(unittest.TestCase):
         self.assertEqual(res, expected)
 
     def test_multiple_terminals(self):
-        rules = {
-            'start': [('a b c', get_string_of_instructions_reducer('start1'))],
-            'other_start': [('b c d', get_string_of_instructions_reducer('other_start1'))]
-        }
+        class Parser(P):
+            terminal = 'start', 'other_start'
 
-        parser = Parser(rules=rules, terminal=['start', 'other_start'])
-        parser.build()
+            start = 'a b c', get_string_of_instructions_reducer('start1')
+            other_start = 'b c d', get_string_of_instructions_reducer('other_start1')
+
+        parser = Parser()
 
         start_stream = generate_token_stream(
             ('a', 'a'),
@@ -288,13 +282,13 @@ class ParserTestAdvanced(unittest.TestCase):
         self.assertEqual(res, expected)
 
     def test_multiple_overlapping_terminals(self):
-        rules = {
-            'start': [('a other_start', get_string_of_instructions_reducer('start1'))],
-            'other_start': [('b c', get_string_of_instructions_reducer('other_start1'))]
-        }
+        class Parser(P):
+            terminal = 'start', 'other_start'
 
-        parser = Parser(rules=rules, terminal=['start', 'other_start'])
-        parser.build()
+            start = 'a other_start', get_string_of_instructions_reducer('start1')
+            other_start = 'b c', get_string_of_instructions_reducer('other_start1')
+
+        parser = Parser()
 
         start_stream = generate_token_stream(
             ('a', 'a'),
@@ -330,21 +324,16 @@ class ParserTestConflicts(unittest.TestCase):
 
     def test_reduce_reduce_basic(self):
 
-        rules = {
-            'start': [('a', placeholder_reducer),
-                      ('b', placeholder_reducer)],
-            'a': [
-                ('A B C', placeholder_reducer)
-            ],
-            'b': [
-                ('A B C', placeholder_reducer)
-            ]
-        }
+        class Parser(P):
+            terminal = 'start'
 
-        parser = Parser(rules=rules, terminal='start')
+            start = ('a', placeholder_reducer),\
+                    ('b', placeholder_reducer)
+            a = 'A B C', placeholder_reducer
+            b = 'A B C', placeholder_reducer
 
         try:
-            parser.build()
+            parser = Parser()
             self.assertTrue(False, msg='Parser did not detect reduce/reduce conflict')
 
         except GrammarError as e:
@@ -353,25 +342,21 @@ class ParserTestConflicts(unittest.TestCase):
 
     def test_reduce_reduce_empty_list(self):
 
-        rules = {
-            'list': [
-                ('list_of_letters', placeholder_reducer),
-                ('list_of_numbers', placeholder_reducer)
-            ],
-            'list_of_letters': [
-                ('', placeholder_reducer),
-                ('LETTER list_of_letters', placeholder_reducer)
-            ],
-            'list_of_numbers': [
-                ('', placeholder_reducer),
-                ('NUMBER list_of_numbers', placeholder_reducer)
-            ]
-        }
+        class Parser(P):
+            terminal = 'list'
 
-        parser = Parser(rules=rules, terminal='list')
+            list =\
+                ('list_of_letters', placeholder_reducer),\
+                ('list_of_numbers', placeholder_reducer)
+            list_of_letters =\
+                ('', placeholder_reducer),\
+                ('LETTER list_of_letters', placeholder_reducer)
+            list_of_numbers =\
+                ('', placeholder_reducer),\
+                ('NUMBER list_of_numbers', placeholder_reducer)
 
         try:
-            parser.build()
+            parser = Parser()
             self.assertTrue(False, msg='Parser did not detect reduce/reduce conflict')
 
         except GrammarError as e:
@@ -380,39 +365,31 @@ class ParserTestConflicts(unittest.TestCase):
 
     def test_fixed_empty_list(self):
 
-        rules = {
-            'list': [
-                ('list_of_letters', placeholder_reducer),
-                ('list_of_numbers', placeholder_reducer)
-            ],
-            'list_of_letters': [
-                ('LETTER list_of_letters?', placeholder_reducer)
-            ],
-            'list_of_numbers': [
-                ('NUMBER list_of_numbers?', placeholder_reducer)
-            ]
-        }
+        class Parser(P):
+            terminal = 'list'
 
-        parser = Parser(rules=rules, terminal='list')
+            list =\
+                ('list_of_letters', placeholder_reducer),\
+                ('list_of_numbers', placeholder_reducer)
+            list_of_letters = 'LETTER list_of_letters?', placeholder_reducer
+            list_of_numbers = 'NUMBER list_of_numbers?', placeholder_reducer
 
         try:
-            parser.build()
+            parser = Parser()
         except GrammarError:
             self.assertTrue(False, msg='Parser detected wrong conflict')
 
     def test_shift_reduce_dangling_else(self):
 
-        rules = {
-            'stat': [
-                ('IF stat ELSE stat', placeholder_reducer),
-                ('IF stat', placeholder_reducer)
-            ]
-        }
+        class Parser(P):
+            terminal = 'stat'
 
-        parser = Parser(rules=rules, terminal='stat')
+            stat =\
+                ('IF stat ELSE stat', placeholder_reducer),\
+                ('IF stat', placeholder_reducer)
 
         try:
-            parser.build()
+            parser = Parser()
             self.assertTrue(False, msg='Parser did not detect shift/reduce conflict')
 
         except GrammarError as e:
@@ -421,36 +398,32 @@ class ParserTestConflicts(unittest.TestCase):
 
     def test_fixed_dangling_else(self):
 
-        rules = {
-            'stat': [
-                ('IF stat ELSE stat SEMICOLON', placeholder_reducer),
-                ('IF stat SEMICOLON', placeholder_reducer)
-            ]
-        }
+        class Parser(P):
+            terminal = 'stat'
 
-        parser = Parser(rules=rules, terminal='stat')
+            stat = \
+                ('IF stat ELSE stat SEMICOLON', placeholder_reducer),\
+                ('IF stat SEMICOLON', placeholder_reducer)
 
         try:
-            parser.build()
+            parser = Parser()
         except GrammarError:
             self.assertTrue(False, msg='Parser detected wrong conflict')
 
     def test_shift_reduce_arithmetic(self):
 
-        rules = {
-            'exp': [
-                ('exp PLUS exp', placeholder_reducer),
-                ('exp TIMES exp', placeholder_reducer),
-                ('LEFT_PAR exp RIGHT_PAR', placeholder_reducer),
-                ('NUMBER', placeholder_reducer),
-                ('VARIABLE', placeholder_reducer)
-            ]
-        }
+        class Parser(P):
+            terminal = 'exp'
 
-        parser = Parser(rules=rules, terminal='exp')
+            exp =\
+                ('exp PLUS exp', placeholder_reducer),\
+                ('exp TIMES exp', placeholder_reducer),\
+                ('LEFT_PAR exp RIGHT_PAR', placeholder_reducer),\
+                ('NUMBER', placeholder_reducer),\
+                ('VARIABLE', placeholder_reducer)
 
         try:
-            parser.build()
+            parser = Parser()
             self.assertTrue(False, msg='Parser did not detect shift/reduce conflict')
 
         except GrammarError as e:
@@ -460,41 +433,36 @@ class ParserTestConflicts(unittest.TestCase):
 
     def test_fixed_arithmetic(self):
 
-        rules = {
-            'exp': [
-                ('factor PLUS factor', placeholder_reducer),
-                ('factor', placeholder_reducer)
-            ],
-            'factor': [
-                ('atomic TIMES atomic', placeholder_reducer),
-                ('atomic', placeholder_reducer)
-            ],
-            'atomic': [
-                ('NUMBER', placeholder_reducer),
-                ('VARIABLE', placeholder_reducer),
-                ('LEFT_PAR exp RIGHT_PAR', placeholder_reducer)
-            ]
-        }
+        class Parser(P):
+            terminal = 'exp'
 
-        parser = Parser(rules=rules, terminal='exp')
+            exp =\
+                ('factor PLUS factor', placeholder_reducer),\
+                ('factor', placeholder_reducer)
+            factor =\
+                ('atomic TIMES atomic', placeholder_reducer),\
+                ('atomic', placeholder_reducer)
+            atomic =\
+                ('NUMBER', placeholder_reducer),\
+                ('VARIABLE', placeholder_reducer),\
+                ('LEFT_PAR exp RIGHT_PAR', placeholder_reducer)
 
         try:
-            parser.build()
+            parser = Parser()
         except GrammarError:
             self.assertTrue(False, msg='Parser detected wrong conflict')
 
     def test_reduction_cycle(self):
 
-        rules = {
-            'A': [('B', placeholder_reducer)],
-            'B': [('C', placeholder_reducer)],
-            'C': [('A', placeholder_reducer)]
-        }
+        class Parser(P):
+            terminal = 'A'
 
-        parser = Parser(rules=rules, terminal='A')
+            A = 'B', placeholder_reducer
+            B = 'C', placeholder_reducer
+            C = 'A', placeholder_reducer
 
         try:
-            parser.build()
+            parser = Parser()
             self.assertTrue(False, msg='Parser did not detect reduce cycle')
 
         except GrammarError as e:
@@ -504,58 +472,29 @@ class ParserTestConflicts(unittest.TestCase):
 class ParserTestBuild(unittest.TestCase):
 
     def test_terminal_token_given(self):
-        rules = {
-            'A': [('a', placeholder_reducer)],
-        }
+        class Parser(P):
+            A = 'a', placeholder_reducer
 
-        parser = Parser(rules=rules)
-        self.assertRaises(ParserBuildError, parser.build)
+        self.assertRaises(ParserBuildError, Parser)
 
     def test_terminal_in_rules(self):
-        rules = {
-            'A': [('a', placeholder_reducer)],
-        }
+        class Parser(P):
+            terminal = 'B'
 
-        parser = Parser(rules=rules, terminal='B')
-        self.assertRaises(ParserBuildError, parser.build)
+            A = 'a', placeholder_reducer
 
-    def test_rule_structure(self):
-        rules = {
-            'A': ('a', placeholder_reducer),
-        }
-
-        self.assertRaises(ParserError, Parser, rules=rules, terminal='A')
-
-    def test_must_build_before_parse(self):
-        rules = {
-            'dummy_rule': [('a b c', placeholder_reducer)]
-        }
-
-        parser = Parser(rules=rules, terminal='dummy_rule')
-
-        tk = generate_token('a')
-
-        self.assertRaises(ParserError, parser.parse, tk)
-
-    def test_must_build_before_end(self):
-        rules = {
-            'dummy_rule': [('a b c', placeholder_reducer)]
-        }
-
-        parser = Parser(rules=rules, terminal='dummy_rule')
-
-        self.assertRaises(ParserError, parser.end)
+        self.assertRaises(ParserBuildError, Parser)
 
 
 class ParserTestReset(unittest.TestCase):
 
     def test_reset(self):
-        rules = {
-            'start':  [('a b c', lambda *args: 'success')]
-        }
+        class Parser(P):
+            terminal = 'start'
 
-        parser = Parser(rules=rules, terminal='start')
-        parser.build()
+            start = 'a b c', lambda *args: 'success'
+
+        parser = Parser()
 
         partial_stream = generate_token_stream('a', 'b')
         full_stream = generate_token_stream('a', 'b', 'c')
@@ -585,8 +524,9 @@ class ParserTestSave(ParserTestBasic):
         cls.parser.save(cls.parser_filename)
 
     def tearDown(self):
-        self.__class__.parser = Parser.load(self.parser_filename)
+        self.__class__.parser = P.load(self.parser_filename)
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+
