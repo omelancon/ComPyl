188b216db929a9f7ca22cbc0a58c68677fc45f28
omelancon <ol.melancon@gmail.com>
2018-09-14 17:09:36 -0400

    BUMP MINOR

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(name='compyl',
       packages=find_packages(exclude=("tests",)),
-      version='0.1.3',
+      version='0.2.0',
       description='Python lexing-parsing tool',
       author='Olivier Melancon',
       author_email='ol.melancon@gmail.com',
       url='https://github.com/omelancon/ComPyl',
-      download_url='https://github.com/omelancon/ComPyl/archive/v0.1.3.tar.gz',
+      download_url='https://github.com/omelancon/ComPyl/archive/v0.2.0.tar.gz',
       install_requires=[
         'dill',
       ],
